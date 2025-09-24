170565241f2e5e1aadd05d2e4d41c914fbe3d02b
kalyaniyergude <kalyaniyergude12@gmail.com>
2025-09-24 22:30:58 +0530

    first commit

---
+++ src/main/java/com/edureka/dev_project/DevProjectApplication.java
@@ -0,0 +1,22 @@
+package com.edureka.dev_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DevProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevProjectApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello() {
+		return "<h1> Welcome to edurekha Devops course</h1>";
+
+	}
+
+}
